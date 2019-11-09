aa961b9d4e68078e47e2c1b45c8a911cf35de009
Levit Osei Kanner <lkanner21@gmail.com>
2019-11-09 14:08:45 +0000

    Updates Missions View

--- MoonShot/MissionsView.swift
+++ MoonShot/MissionsView.swift
@@ -11,6 +11,13 @@ import SwiftUI
 struct MissionsView: View {
     //Properties
     let mission: Mission
+    let astronauts: [CrewMember]
+    
+    //Resolved Astronauts
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
     
     var body: some View {
         GeometryReader{geometry in
@@ -26,11 +33,52 @@ struct MissionsView: View {
                         .padding()
                     
                     Spacer(minLength: 25)
-                    
+                    Section(header: Text("Crew")
+                        .font(.headline)
+                        .underline()) {
+
+                    ForEach(self.astronauts , id: \.role){crewMember in
+                        HStack{
+                            Image("\(crewMember.astronaut.id)")
+                            .resizable()
+                                .frame(width: 93 , height: 70)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.primary, lineWidth: 1))
+                            
+                            VStack(alignment: .leading){
+                                Text("\(crewMember.astronaut.name)")
+                                    .font(.headline)
+                                Text("\(crewMember.role)")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(crewMember.role.hasPrefix("Commander") ? .red : .green)
+                            }
+                            Spacer()
+                                
+                        }
+                        .padding(.horizontal)
+                        
+                    }
+                    }
                 }
             }
         }
         .navigationBarTitle("\(mission.displayName)" , displayMode: .inline)
+    }
+    
+    init(mission: Mission , astronauts: [Astronaut]){
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }){
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            }else{
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
     }
 }
 
@@ -38,6 +86,6 @@ struct MissionsView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     static var previews: some View {
-        MissionsView(mission: missions[0])
+        MissionsView(mission: missions[0], astronauts: astronauts)
     }
 }
